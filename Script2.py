75bcd9d943c87ba0a9bb5b39e11201cfa08f3181
tongramt <grantth@tcd.ie>
2021-04-20 15:44:06 +0100

    Changed Datasets are added and cleaned

--- Script2.py
+++ Script2.py
@@ -2,24 +2,63 @@
 import pandas as pd
 from pyjstat import pyjstat
 
-girls_URL = 'https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/VSA60/JSON-stat/1.0/'
-boys_URL = 'https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/VSA50/JSON-stat/1.0/'
-birth_rates_URL = 'https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/VSB14/JSON-stat/1.0/'
+marriage_URL = 'https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/VSA41/JSON-stat/1.0/'
+income_URL = 'https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/SIA51/JSON-stat/1.0/'
+birth_URL = 'https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/VSA15/JSON-stat/1.0/'
 
 # read from json-stat
-girls = pyjstat.Dataset.read(girls_URL)
-boys = pyjstat.Dataset.read(boys_URL)
-birth_rates = pyjstat.Dataset.read(birth_rates_URL)
+marriage = pyjstat.Dataset.read(marriage_URL)
+income = pyjstat.Dataset.read(income_URL)
+birth = pyjstat.Dataset.read(birth_URL)
 
 # write to dataframe
-girls = girls.write('dataframe')
-boys = boys.write('dataframe')
-birth_rates = birth_rates.write('dataframe')
+marriage = marriage.write('dataframe')
+income = income.write('dataframe')
+birth = birth.write('dataframe')
 
-both_genders = pd.concat([girls, boys])
-full_data = pd.concat([both_genders, birth_rates])
-print(full_data.head())
+marriage['Year'] = pd.to_numeric(marriage['Year'])
+marriage = marriage[marriage.Year.between(2012, 2021)]
+income['Year'] = pd.to_numeric(income['Year'])
+income = income[income.Year.between(2012, 2021)]
+birth['Year'] = pd.to_numeric(birth['Year'])
+birth = birth[birth.Year.between(2012, 2021)]
 
-
-
-
+print()
+#
+# # Source for code: https://dash.plotly.com/layout
+# #  Need to install dash and plotly
+# # visit http://127.0.0.1:8050/ in your web browser.
+#
+# import dash
+# import dash_core_components as dcc
+# import dash_html_components as html
+# import plotly.express as px
+#
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+#
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+#
+# # assume you have a "long-form" data frame
+# # see https://plotly.com/python/px-arguments/ for more options
+# df = full_data
+#
+# fig = px.bar(df, x="Names", y="value", barmode="group")
+#
+# app.layout = html.Div(children=[
+#     html.H1(children='My First Dash'),
+#
+#     html.Div(children='''
+#         Dash: A web application framework for Python.
+#     '''),
+#
+#     dcc.Graph(
+#         id='example-graph',
+#         figure=fig
+#     )
+# ])
+#
+# if __name__ == '__main__':
+#     app.run_server(debug=True, use_reloader=False)
+#
+#
+#
