289c07b4dd289e3aa16df344984ccb15f9d4e463
tongramt <grantth@tcd.ie>
2021-04-20 11:50:47 +0100

    Script for importing Json stats from URL

--- Script2.py
+++ Script2.py
@@ -1,1 +1,14 @@
 # Code for part 1: Script 2
+import urllib3
+import pandas as pd
+import pyjstat from pyjstat
+
+EXAMPLE_URL = 'http://json-stat.org/samples/galicia.json'
+
+# read from json-stat
+dataset = pyjstat.Dataset.read(EXAMPLE_URL)
+
+# write to dataframe
+df = dataset.write('dataframe')
+print(df)
+
