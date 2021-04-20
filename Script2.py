78690cf7c79781df53c324b76578c44c39609901
nacarney <nacarney@tcd.ie>
2021-04-21 00:04:51 +0100

    Dashboard

--- Script2.py
+++ Script2.py
@@ -2,6 +2,12 @@
 import pandas as pd
 from pyjstat import pyjstat
 import openpyxl
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+from dash.dependencies import Input, Output
+import matplotlib.pyplot as plt
 
 # the URLs of the json-stat files to be used
 marriage_URL = 'https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/VSA41/JSON-stat/1.0/'
@@ -38,49 +44,113 @@ income = income.pivot(index=["Year", 'Region'], columns="Statistic", values="val
 b_m_data = pd.merge(marriage, birth, how='outer', on=['Year', 'Region'])
 full_data = pd.merge(b_m_data, income, how='outer', on=['Year', 'Region'])
 
-datatoexcel = pd.ExcelWriter('full_data.xlsx')
-# write DataFrame to excel
-full_data.to_excel(datatoexcel)
+# print(full_data[full_data.index.get_level_values('Year').isin([2013])].columns)
 
-# save the excel
-datatoexcel.save()
+# dff = pd.DataFrame(full_data[full_data.index.get_level_values('Region').isin(['Dublin'])]['Average Age of Bride'])
+# print(dff.reset_index()['Average Age of Bride'])
 
 
+
+
+
+# print(full_data.index.get_level_values('Year').unique().values)
+
+
+
+
+
+#print(full_data.index.get_level_values('Region').unique())
+#print(full_data[full_data.index.isin(['Dublin'], level=1)]['Average Age of Bride'])
+# print(full_data[full_data.index.get_level_values('Region').isin(['Dublin', 'Midland'])])
+# datatoexcel = pd.ExcelWriter('full_data.xlsx')
+# # write DataFrame to excel
+# full_data.to_excel(datatoexcel)
 #
-# # Source for code: https://dash.plotly.com/layout
-# #  Need to install dash and plotly
-# # visit http://127.0.0.1:8050/ in your web browser.
+# # save the excel
+# datatoexcel.save()
 #
-# import dash
-# import dash_core_components as dcc
-# import dash_html_components as html
-# import plotly.express as px
-#
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-#
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-#
-# # assume you have a "long-form" data frame
-# # see https://plotly.com/python/px-arguments/ for more options
-# df = full_data
-#
-# fig = px.bar(df, x="Names", y="value", barmode="group")
-#
+#Create the Dash app
+app = dash.Dash()
+
 # app.layout = html.Div(children=[
-#     html.H1(children='My First Dash'),
-#
-#     html.Div(children='''
-#         Dash: A web application framework for Python.
-#     '''),
-#
-#     dcc.Graph(
-#         id='example-graph',
-#         figure=fig
-#     )
+#     html.H1(children='Marriage, Income and Birth Statistics Dashboard'),
+#     dcc.Dropdown(id='year-dropdown',
+#                  options=[{'label': i, 'value': i}
+#                           for i in full_data.index.get_level_values('Year').unique()],
+#                  value='2018'),
+#     dcc.Graph(id='stats-graph')
 # ])
 #
+# #Set up the callback function
+# @app.callback(
+#     Output(component_id='stats-graph', component_property='figure'),
+#     Input(component_id='year-dropdown', component_property='value')
+# )
+# def update_graph(selected_year):
+#     filtered_year = full_data[full_data.index.get_level_values('Year').isin([selected_year])]
+#     line_fig = px.bar(filtered_year,
+#                        x=full_data.index.get_level_values('Region').unique(),
+#                        y=
+#                        full_data[full_data.index.get_level_values('Year').isin([selected_year])],
+#                        title=f'Stats in {selected_year}')
+#     return line_fig
+#
+# # Run local server
 # if __name__ == '__main__':
-#     app.run_server(debug=True, use_reloader=False)
-#
-#
-#
+#     app.run_server(debug=True)
+
+app.layout = html.Div(children=[
+
+    # the filter div
+    html.Div([
+
+        # geo
+        html.Div([
+            dcc.Dropdown(
+                id='region',
+                options=[{'label': i, 'value': i} for i in
+                         full_data.index.get_level_values('Region').unique()],
+                value='Dublin',
+                placeholder="Select a Region"),
+
+                dcc.Dropdown(
+                    id='statistic',
+                    value = 'Average Age of Bride',
+                    multi=False),
+
+                html.Hr(),
+
+            ])
+        ]),
+    dcc.Graph(id='stats-graph')
+    ])
+
+
+@app.callback(
+    Output('statistic', 'options'),
+    [Input('region', 'value')])
+def get_statistic(region_unit):
+        return \
+            [{'label': i, 'value': i} for i in full_data[full_data.index.get_level_values('Region').isin([region_unit])]
+                .columns]
+
+@app.callback(
+    Output(component_id='stats-graph', component_property='figure'),
+    Input(component_id='region', component_property='value'),
+    Input(component_id='statistic', component_property='value')
+)
+def update_graph(region, statistic):
+    dff = full_data[full_data.index.get_level_values('Region').isin([region])][statistic]
+    line_fig = px.line(dff,
+                       x= dff.reset_index()['Year'],
+                       y= dff.reset_index()[statistic],
+                       title=f'{statistic} in {region}'
+                       )
+    line_fig.update_xaxes(title_text = "Year")
+
+    line_fig.update_yaxes(title_text = statistic)
+
+    return line_fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
